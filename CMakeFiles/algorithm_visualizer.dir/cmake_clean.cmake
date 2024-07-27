e07d16760628959dcdea1973850f8ebf7f97f254
Kavyadeep Dev <devkavyadeep@gmail.com>
2024-07-27 22:10:44 +0530

    add: cmake config and .clang-format

---
+++ CMakeFiles/algorithm_visualizer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm_visualizer.dir/main.cpp.o"
+  "CMakeFiles/algorithm_visualizer.dir/main.cpp.o.d"
+  "bin/Linux64/debug/algorithm_visualizer"
+  "bin/Linux64/debug/algorithm_visualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithm_visualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
